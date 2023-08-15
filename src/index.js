8d40ada687b6daa7729a8a36167fb835f44def57
Multichiki22 <myguelangel12@gmail.com>
2023-08-15 17:56:26 -0500

    added deploy direction option

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import  {BrowserRouter} from "react-router-dom";
+const deployDirection = process.env.REACT_APP_DEPLOY_URL
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={`/${deployDirection}`}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
